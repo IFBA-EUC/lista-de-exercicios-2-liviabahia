a347d6f18ad361ce61112d77727edbfe53c66d12
Lívia Bahia <88677754+liviabahia@users.noreply.github.com>
2021-08-22 18:04:54 -0300

    Create questao5.java

---
+++ questao5.java
@@ -0,0 +1,9 @@
+public class questao5 {
+  public static void main(String[] args) {
+    for (int l = 1; l <= 10; l++) {
+        for (int m = 1; m <= 10; m++) {
+      System.out.println(l + "x" + m + "=" + (l*m));
+      }
+    }
+  }
+}
